7bebe0c8738fa2a6e9576ad2bb8845eb2c799f9a
unknown <citythree.11798@gmail.com>
2021-10-19 21:42:26 +0700

    update autoplay slider of section2, button prev and next

--- index.js
+++ index.js
@@ -128,12 +128,20 @@ let sections = document.querySelectorAll("section")
 
 /* section2-slider */
 var swiper = new Swiper(".section2-list", {
-    slidesPerView: 2,
-    spaceBetween: 10,
+    slidesPerView: 3,
+    spaceBetween: 25,
     centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
   });
 
